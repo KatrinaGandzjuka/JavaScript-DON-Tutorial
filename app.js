010470eddd9df5c05386c905d7ad27e4ad19645a
KatrinaGandzjuka <katrina.gandzjuk@icloud.com>
2021-03-09 16:08:42 +0200

    Tutorial 12

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const list = document.querySelector('#book-list ul');
+const forms = document.forms;
 
 // delete books
 list.addEventListener('click', (e) => {
@@ -8,11 +9,24 @@ list.addEventListener('click', (e) => {
   }
 });
 
-// add book-list
+// add books
 const addForm = forms['add-book'];
-
 addForm.addEventListener('submit', function(e){
   e.preventDefault();
+
+  // create elements
   const value = addForm.querySelector('input[type="text"]').value;
-  console.log(value);
+  const li = document.createElement('li');
+  const bookName = document.createElement('span');
+  const deleteBtn = document.createElement('span');
+
+  // add text content
+  bookName.textContent = value;
+  deleteBtn.textContent = 'delete';
+
+  // append to DOM
+  li.appendChild(bookName);
+  li.appendChild(deleteBtn);
+  list.appendChild(li);
+  //list.insertBefore(li, list.querySelector('li:first-child'));
 });
